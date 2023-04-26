c01c8dc3a7132dee8229ab533deba15ea1dc1735
Zeynep Kaya <zk.14.hk@gmail.com>
2023-04-26 12:33:43 +0300

    v:7 aslinda_v3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -8,15 +8,11 @@ public class AtmApp {
 //version1
         System.out.println("java is easy!");
         // version2
-        System.out.println("learn java earn money");
-        //version3
-        System.out.println("javayi ogrenmek cok kolay");
-        //version4
 
-        System.out.println("git ogrenmek zevkli");
 
-//version 6
-        //version6
+
+
+
 
 
 
