228b682df380bebb17788f7b4750dacc70413800
Zeynep Kaya <zk.14.hk@gmail.com>
2023-04-26 12:00:47 +0300

    v1

---
+++ src/AtmApp.java
@@ -0,0 +1,30 @@
+public class AtmApp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("bu bir atm projesidir.");
+
+//version1
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
